a275dc334915b0ac0698374ec2e1ca2264142ea0
BattlePoap <alapetite.mathieu@gmail.com>
2018-04-02 12:36:11 +0200

    assigning keys

---
+++ assets/patatap-keys.js
@@ -0,0 +1,158 @@
+var keyData = {
+            a: {
+                color: "purple",
+                sound: new Howl ({
+                    src: ['assets/sounds/bubbles.mp3']
+                })
+            },
+            b: {
+                color: "green",
+                sound: new Howl({
+                    src: ['assets/sounds/clay.mp3']
+                })
+            },
+            c: {
+                color: "yellow",
+                sound: new Howl({
+                    src: ['assets/sounds/confetti.mp3']
+                })
+            },
+            d: {
+                color: "steelblue",
+                sound: new Howl({
+                    src: ['assets/sounds/corona.mp3']
+                })
+            },
+            e: {
+                color: "wheat",
+                sound: new Howl({
+                    src: ['assets/sounds/dotted-spiral.mp3']
+                })
+            },
+            f: {
+                color: "yellow",
+                sound: new Howl({
+                    src: ['assets/sounds/flash-1.mp3']
+                })
+            },
+            g: {
+                color: "red",
+                sound: new Howl({
+                    src: ['assets/sounds/flash-2.mp3']
+                })
+            },
+            h: {
+                color: "cyan",
+                sound: new Howl({
+                    src: ['assets/sounds/flash-3.mp3']
+                })
+            },
+            i: {
+                color: "lightgray",
+                sound: new Howl({
+                    src: ['assets/sounds/glimmer.mp3']
+                })
+            },
+            j: {
+                color: "white",
+                sound: new Howl({
+                    src: ['assets/sounds/moon.mp3']
+                })
+            },
+            k: {
+                color: "lime",
+                sound: new Howl({
+                    src: ['assets/sounds/pinwheel.mp3']
+                })
+            },
+            l: {
+                color: "orange",
+                sound: new Howl({
+                    src: ['assets/sounds/piston-1.mp3']
+                })
+            },
+            m: {
+                color: "blue",
+                sound: new Howl({
+                    src: ['assets/sounds/piston-2.mp3']
+                })
+            },
+            n: {
+                color: "magenta",
+                sound: new Howl({
+                    src: ['assets/sounds/piston-3.mp3']
+                })
+            },
+            o: {
+                color: "lightgreen",
+                sound: new Howl({
+                    src: ['assets/sounds/prism-1.mp3']
+                })
+            },
+            p: {
+                color: "grey",
+                sound: new Howl({
+                    src: ['assets/sounds/prism-2.mp3']
+                })
+            },
+            q: {
+                color: "sandybrown",
+                sound: new Howl({
+                    src: ['assets/sounds/prism-3.mp3']
+                })
+            },
+            r: {
+                color: "springgreen",
+                sound: new Howl({
+                    src: ['assets/sounds/splits.mp3']
+                })
+            },
+            s: {
+                color: "seashell",
+                sound: new Howl({
+                    src: ['assets/sounds/squiggle.mp3']
+                })
+            },
+            t: {
+                color: "slateblue",
+                sound: new Howl({
+                    src: ['assets/sounds/strike.mp3']
+                })
+            },
+            u: {
+                color: "honeydew",
+                sound: new Howl({
+                    src: ['assets/sounds/suspension.mp3']
+                })
+            },
+            v: {
+                color: "indianred",
+                sound: new Howl({
+                    src: ['assets/sounds/timer.mp3']
+                })
+            },
+            w: {
+                color: "indigo",
+                sound: new Howl({
+                    src: ['assets/sounds/ufo.mp3']
+                })
+            },
+            x: {
+                color: "hotpink",
+                sound: new Howl({
+                    src: ['assets/sounds/veil.mp3']
+                })
+            },
+            y: {
+                color: "chocolate",
+                sound: new Howl({
+                    src: ['assets/sounds/wipe.mp3']
+                })
+            },
+            z: {
+                color: "mintcream",
+                sound: new Howl({
+                    src: ['assets/sounds/zig-zag.mp3']
+                })
+            }
+        }
